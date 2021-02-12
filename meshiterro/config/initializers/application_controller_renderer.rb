d430e0735087ddf076b6f337d23ee6c53c127c4c
potetakoyaki <potetakoyaki@gmail.com>
2021-02-12 10:28:21 +0000

    [finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b2ff15d7c5058b41cf9b384bcf7f5297c9f6f55c83069c39f6c01067ac85b003a48fd3323e0f6cc5fb5c11a5856a4508213f36e3b652b20529eb2aee93bd1a7f'
