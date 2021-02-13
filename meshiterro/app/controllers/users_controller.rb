d95bbc52374ecdfbf3f16a9b9c2ff0e7013627b3
potetakoyaki <potetakoyaki@gmail.com>
2021-02-13 09:16:09 +0000

    [finish]4-5

--- meshiterro/app/controllers/users_controller.rb
+++ meshiterro/app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:id]).reverse_order
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @usr = update(user_params)
+    redirect to user_path(@user.id)
+  end
+  
+  private
+  def user_params
+    params.requier(:user).permit(:name, :profile_image)
   end
 end
